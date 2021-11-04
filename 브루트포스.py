b3739634d44228141bc0e32401eb3220d6d086b8
khs <69028828+happysang@users.noreply.github.com>
2021-11-05 04:12:01 +0900

    Update 브루트포스.py

--- 브루트포스.py
+++ 브루트포스.py
@@ -19,6 +19,34 @@ print(res)
 
 
 # 2231번
+#풀이1)
+def check(num):
+    res = []
+    for a in range(10):
+        for b in range(10):
+            for c in range(10):
+                for d in range(10):
+                    for e in range(10):
+                        for f in range(10):
+                            if num == int(str(a)+str(b)+str(c)+str(d)+str(e)+str(f))+a+b+c+d+e+f:
+                                return int(str(a)+str(b)+str(c)+str(d)+str(e)+str(f))
+    return 0
+
+print( check(int(input())) )
+
+#풀이2
+def check(n):
+    min_n = n - len(str(n))*9
+    for x in range(min_n, n):
+        if x < 0:
+            continue
+        s = x
+        for i in str(x):
+            s += int(i)
+        if s == n:
+            return x
+    return 0
+print(check(int(input())))
 
 
 
