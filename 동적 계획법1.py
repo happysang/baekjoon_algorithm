d00e700eeb7c9473cd8a36300c080f23ba073a97
khs <69028828+happysang@users.noreply.github.com>
2021-12-23 17:44:46 +0900

    Update 동적 계획법1.py

---
+++ 동적 계획법1.py
@@ -0,0 +1,93 @@
+# 1003번
+import sys
+res = [[0,0] for _ in range(41)]
+nums = [0 for _ in range(41)]
+
+def fibo(n):
+    if n == 0:
+        nums[0] = 0
+        res[0][0] = 1
+        return 0
+    
+    elif n == 1:
+        nums[1] = 1
+        res[1][1] = 1
+        return 1
+        
+    elif nums[n] != 0:
+        return nums[n]
+    
+    else:
+        nums[n] = nums[n-1] + nums[n-2]
+        res[n][0] = res[n-1][0] + res[n-2][0]
+        res[n][1] = res[n-1][1] + res[n-2][1] 
+        return fibo(n-1) + fibo(n-2)
+
+
+num = int(sys.stdin.readline())
+inp = []
+for _ in range(num):
+    inp.append(int(sys.stdin.readline()))
+    
+for i in range(max(inp)+1):
+    fibo(i)
+    
+for i in inp:
+    print(res[i][0], res[i][1])
+    
+    
+
+# 9184번
+
+
+
+# 1904번
+
+
+
+# 9461번
+
+
+
+# 1149번
+
+
+
+# 1932번
+
+
+
+# 2579번
+
+
+
+
+# 1463번
+
+
+
+# 10844번
+
+
+
+# 2156번
+
+
+
+# 11054번
+
+
+
+# 2565번
+
+
+
+# 9251번
+
+
+
+# 1912번
+
+
+
+# 12865번
