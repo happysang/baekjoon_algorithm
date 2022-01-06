f0232fb44530274e5fb0b12d89804ecca02173eb
khs <69028828+happysang@users.noreply.github.com>
2022-01-07 02:57:40 +0900

    Update 그리디 알고리즘.py

--- 그리디 알고리즘.py
+++ 그리디 알고리즘.py
@@ -19,6 +19,23 @@ print(cnt)
 
 
 #1931번
+num = int(input())
+time = []
+
+for _ in range(num):
+  time.append(list(map(int, input().split())))
+
+time = sorted(time, key= lambda a: (a[1],a[0]))
+
+last,cnt = 0 , 0
+
+for i, j in time:
+  if i >= last:
+    cnt += 1
+    last = j
+
+print(cnt)
+
 
 
 #11399번
