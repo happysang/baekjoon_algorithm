3f91f519d50d29ccff3a32821fd197bb0e124d63
khs <69028828+happysang@users.noreply.github.com>
2022-01-21 17:51:44 +0900

    Update 분할정복.py

--- 분할정복.py
+++ 분할정복.py
@@ -42,6 +42,43 @@ print(res2)
 
 
 #1992
+num = int(input())
+
+lis = []
+for x in range(num):
+    temp = []
+    for i in input():
+        temp.append(int(i))
+    lis.append(temp)
+
+
+def sol(num,lis,flag):
+    temp = set()
+    for x in lis:
+        for i in x:
+            temp.add(i)
+            if len(temp) == 2:
+                flag = 2
+                break
+            
+    if flag == 2:
+        lis1, lis2, lis3, lis4 = [],[],[],[]
+        for x in range(num):
+            if x < num//2:
+                lis1.append(lis[x][:num//2])
+                lis2.append(lis[x][num//2:])
+            else:
+                lis3.append(lis[x][:num//2])
+                lis4.append(lis[x][num//2:])
+        return "("+sol(num//2,lis1,0)+sol(num//2,lis2,0)+sol(num//2,lis3,0)+sol(num//2,lis4,0)+")"
+    elif list(temp)[0] == 1:
+        return "1"
+    else:
+        return "0"
+
+print(sol(num,lis,0))
+
+
 
 #1780
 
