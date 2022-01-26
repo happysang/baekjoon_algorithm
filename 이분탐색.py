821ad9ad308ec129b28542c656bd33a871a2d3a6
khs <69028828+happysang@users.noreply.github.com>
2022-01-27 03:52:26 +0900

    Update 이분탐색.py

--- 이분탐색.py
+++ 이분탐색.py
@@ -1,4 +1,33 @@
 #1920
+import sys
+
+num1 = int(input())
+lis = sorted(list(map(int, sys.stdin.readline().split())))
+
+num2 = int(input())
+num_list = list(map(int,sys.stdin.readline().split()))
+
+
+def sol(x):
+    l,r = 0,num1-1
+    
+    while(l<=r):
+        mid = (l+r)//2
+        if x == lis[mid]:
+            print(1)
+            return
+        elif x < lis[mid]:
+            r = mid-1
+        else:
+            l = mid+1
+    print(0)
+    return
+
+for x in num_list:
+    sol(x)
+    
+    
+    
 #10816
 #1654
 #2805
