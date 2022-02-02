1a23ec305a06135561cee1a84539fd2f3503626a
khs <69028828+happysang@users.noreply.github.com>
2022-02-03 02:18:07 +0900

    Update 우선순위 큐.py

--- 우선순위 큐.py
+++ 우선순위 큐.py
@@ -20,5 +20,24 @@ for x in range(num):
         
         
 #11286
+import sys,heapq
+
+heap = []
+heapq.heapify(heap)
+
+num = int(sys.stdin.readline().strip())
+
+for x in range(num):
+    tar = sys.stdin.readline().strip()
+    
+    if tar == '0':
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (abs(int(tar))+(int(tar)*(0.0000000000000000000001)),int(tar)))
+        
+        
 
 #1655
