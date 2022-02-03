db25477c16b29cad8e04fe988d7fa958396b862e
khs <69028828+happysang@users.noreply.github.com>
2022-02-03 19:20:48 +0900

    Update 우선순위 큐.py

--- 우선순위 큐.py
+++ 우선순위 큐.py
@@ -1,5 +1,24 @@
 #11279
+import sys,heapq
 
+heap = []
+heapq.heapify(heap)
+
+num = int(sys.stdin.readline().strip())
+
+for x in range(num):
+    tar = sys.stdin.readline().strip()
+    
+    if tar == '0':
+        if len(heap) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (-int(tar),int(tar)))
+        
+        
+        
 #1927
 import sys,heapq
 
@@ -17,6 +36,7 @@ for x in range(num):
             print(heapq.heappop(heap))
     else:
         heapq.heappush(heap, int(tar))
+        
         
         
 #11286
@@ -40,4 +60,29 @@ for x in range(num):
         
         
 
+
 #1655
+import sys,heapq
+num = int(sys.stdin.readline().strip())
+lheap, rheap = [], []
+heapq.heapify(lheap)
+heapq.heapify(rheap)
+
+
+flag = True
+for x in range(num):
+    tar = int(sys.stdin.readline().strip())
+    if flag:
+        heapq.heappush(lheap, (-tar, tar))
+        flag = False
+    else:
+        heapq.heappush(rheap, tar)
+        flag = True
+        
+    if len(rheap) > 0 and len(lheap):
+        if lheap[0][1] > rheap[0]:
+            r = heapq.heappop(rheap)
+            l = heapq.heappop(lheap)
+            heapq.heappush(lheap, (-r,r))
+            heapq.heappush(rheap, l[1])
+    print(lheap[0][1])
