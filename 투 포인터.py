28bceec68763e1e112cb9065afa8b2e372d2286f
khs <69028828+happysang@users.noreply.github.com>
2022-02-05 02:26:29 +0900

    Update 투 포인터.py

--- 투 포인터.py
+++ 투 포인터.py
@@ -23,6 +23,29 @@ print(cnt)
 
 
 #2470
+import sys
+from collections import deque
+
+num = int(input())
+nums = list(map(int, sys.stdin.readline().split()))
+nums.sort()
+deq = deque(nums)
+min = 10000000000000
+
+while(len(deq) != 1 and len(deq) != 0):
+    res = deq[0] + deq[-1]
+    if abs(res) < abs(min):
+        ans = [deq[0],deq[-1]]
+        min = res
+    if res > 0:
+        deq.pop()
+    elif res < 0:
+        deq.popleft()
+    else:
+        break
+
+print(*ans)
+
 
 #1806
 
