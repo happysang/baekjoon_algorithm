a36ac3db66583b2b2cb66f3f56b7f27594744ab1
khs <69028828+happysang@users.noreply.github.com>
2022-02-04 16:42:24 +0900

    Update 투 포인터.py

--- 투 포인터.py
+++ 투 포인터.py
@@ -1,4 +1,26 @@
 #3273
+import sys
+from collections import deque
+
+num = int(input())
+nums = list(map(int, sys.stdin.readline().split()))
+tar = int(input())
+nums.sort()
+deq = deque(nums)
+cnt = 0
+while(len(deq) != 1 and len(deq) != 0):
+    res = deq[0] + deq[-1]
+    if res > tar:
+        deq.pop()
+    elif res < tar:
+        deq.popleft()
+    else:
+        deq.pop()
+        deq.popleft()
+        cnt += 1
+
+print(cnt)
+
 
 #2470
 
