9307bea95c9948b3b56c38eede1b406d7ccb9472
khs <69028828+happysang@users.noreply.github.com>
2022-04-17 15:14:38 +0900

    Update 부루트포스.py

--- 코딩테스트준비-기초/부루트포스.py
+++ 코딩테스트준비-기초/부루트포스.py
@@ -1,8 +1,30 @@
 # 2309번 - 일곱 난쟁이
+import sys
 
+nums = []
+for _ in range(9):
+    nums.append(int(sys.stdin.readline().strip()))
+
+gap = sum(nums)-100
+
+
+for i in nums:
+    for j in nums:
+        if i == j:
+            continue
+        if gap == i+j:
+            nums.remove(i)
+            nums.remove(j)
+            
+            for i in sorted(nums):
+                print(i)
+            exit()
 
 
 # 3085번 - 사탕 게임
+
+
+
 # 1476번 - 날짜 계산
 # 1107번 - 리모컨
 # 14500번 - 테트로미노
