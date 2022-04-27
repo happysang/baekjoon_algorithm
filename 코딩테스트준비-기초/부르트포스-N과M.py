917f8fce11626edacb53638be3cfdb255d034bb4
khs <69028828+happysang@users.noreply.github.com>
2022-04-27 14:58:25 +0900

    Update 부르트포스-N과M.py

--- 코딩테스트준비-기초/부르트포스-N과M.py
+++ 코딩테스트준비-기초/부르트포스-N과M.py
@@ -34,9 +34,108 @@ sol([0])
 
 
 # 15651번 - N과 M (3)
+import sys
+n, m = map(int, sys.stdin.readline().split())
+
+def sol(lis):   
+    if len(lis) == m:
+        print(' '.join(map(str,lis)))
+        return
+    else:
+        for i in range(1,n+1):
+            lis.append(i)
+            sol(lis)
+            lis.pop()
+sol([])
+
+
 # 15652번 - N과 M (4)
+import sys
+n, m = map(int, sys.stdin.readline().split())
+
+def sol(lis):   
+    if len(lis) == m+1:
+        print(' '.join(map(str,lis[1:])))
+        return
+    else:
+        for i in range(1,n+1):
+            if lis[-1] <= i:
+                lis.append(i)
+                sol(lis)
+                lis.pop()
+sol([0])
+
+
 # 15654번 - N과 M (5)
+import sys
+n, m = map(int, sys.stdin.readline().split())
+nums = sorted(list(map(int, sys.stdin.readline().split())))
+
+def sol(lis):   
+    if len(lis) == m:
+        print(' '.join(map(str,lis)))
+        return
+    else:
+        for i in nums:
+            if i not in lis:
+                lis.append(i)
+                sol(lis)
+                lis.pop()
+sol([])
+
+
 # 15655번 - N과 M (6)
+import sys
+n, m = map(int, sys.stdin.readline().split())
+nums = sorted(list(map(int, sys.stdin.readline().split())))
+
+def sol(lis):   
+    if len(lis) == m+1:
+        print(' '.join(map(str,lis[1:])))
+        return
+    else:
+        for i in nums:
+            if lis[-1] < i:
+                lis.append(i)
+                sol(lis)
+                lis.pop()
+sol([0])
+
+
 # 15656번 - N과 M (7)
+import sys
+n, m = map(int, sys.stdin.readline().split())
+nums = sorted(list(map(int, sys.stdin.readline().split())))
+
+def sol(lis):   
+    if len(lis) == m:
+        print(' '.join(map(str,lis)))
+        return
+    else:
+        for i in nums:
+            lis.append(i)
+            sol(lis)
+            lis.pop()
+sol([])
+
+
 # 15657번 - N과 M (8)
+import sys
+n, m = map(int, sys.stdin.readline().split())
+nums = sorted(list(map(int, sys.stdin.readline().split())))
+
+def sol(lis):   
+    if len(lis) == m+1:
+        print(' '.join(map(str,lis[1:])))
+        return
+    else:
+        for i in nums:
+            if lis[-1] <= i:
+                lis.append(i)
+                sol(lis)
+                lis.pop()
+sol([0])
+
+
+
 # 18290번 - NM과 K (1)
