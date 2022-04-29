7ec382bc9fd49abdb863aba6b8f66687540666e8
khs <69028828+happysang@users.noreply.github.com>
2022-04-30 02:21:52 +0900

    Update 부르트포스-재귀.py

--- 코딩테스트준비-기초/부르트포스-재귀.py
+++ 코딩테스트준비-기초/부르트포스-재귀.py
@@ -48,6 +48,29 @@ call(['1'],0,0)
 
 
 # 14501번 - 퇴사
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+day = []
+money = []
+res = [0] * (n+1)
+
+for _ in range(n):
+    d, m = map(int, input().split())
+    day.append(d)
+    money.append(m)
+    
+for i in range(n-1, -1, -1):
+    if i + day[i] > n:
+        res[i] = res[i+1]
+    else:
+        res[i] = max(res[i+1], money[i]+res[i+day[i]])
+        
+print(res[0])
+
+
+
 # 14889번 - 스타트와 링크
 # 15661번 - 링크와 스타트
 # 2529번 - 부등호
