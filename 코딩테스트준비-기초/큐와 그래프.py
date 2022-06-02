d3bc036c0e34a44d3eb8cdd5766b7c0c099b716b
khs <69028828+happysang@users.noreply.github.com>
2022-06-02 12:10:10 +0900

    Update 큐와 그래프.py

--- 코딩테스트준비-기초/큐와 그래프.py
+++ 코딩테스트준비-기초/큐와 그래프.py
@@ -129,7 +129,85 @@ print(0)
 
 
 # 1260번 - DFS와 BFS
+def dfs(v):
+    print(v, end=' ')
+    visit1[v] = 1
+    for i in sorted(points[v]):
+        if visit1[i] == 0:
+            dfs(i)
+            
+
+from collections import deque
+def bfs(v):
+    lis = deque([v])
+    visit2[v] = 1
+    while(lis):
+        p = lis.popleft()
+        print(p, end=' ')
+        for i in sorted(points[p]):
+            if visit2[i] == 0:
+                lis.append(i)
+                visit2[i] = 1
+                
+
+n,m,v = map(int, input().split())
+points = [[]for _ in range(n+1)]
+
+for i in range(m):
+    i,j = map(int, input().split())
+    points[i].append(j)
+    points[j].append(i)
+
+visit1 = [0 for _ in range(n+1)]
+dfs(v)
+
+print()
+
+visit2 = [0 for _ in range(n+1)]
+bfs(v)
+
+
+
 # 11724번 - 연결 요소의 개수
+import sys
+sys.setrecursionlimit(10**9)
+
+def dfs(v):
+    visit[v] = 1
+    for a in points[v]:
+        if visit[a] == 0:
+            dfs(a)
+    
+from collections import deque
+def bfs(v):
+    lis = deque([v])
+    visit[v] = 1
+    while(lis):
+        p = lis.popleft()
+        for i in points[p]:
+            if visit[i] == 0:
+                lis.append(i)
+                visit[i] = 1
+    
+    
+n,m = map(int, input().split())
+points = [[]for _ in range(n+1)]
+visit = [0 for _ in range(n+1)]
+
+for _ in range(m):
+    i,j = map(int, sys.stdin.readline().split())
+    points[i].append(j)
+    points[j].append(i)
+    
+cnt = 0
+for i in range(1,n+1):
+    if visit[i] == 0:
+        cnt += 1
+        dfs(i) # 혹은 bfs(i)
+        
+print(cnt)
+
+
 # 1707번 - 이분 그래프
 # 2667번 - 단지번호붙이기
 # 2178번 - 미로 탐색
