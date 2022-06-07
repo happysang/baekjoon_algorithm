8daa7915aea4beff421379f241ef2fb92abdfa3d
khs <69028828+happysang@users.noreply.github.com>
2022-06-07 20:12:57 +0900

    Update 그래프와 BFS-BFS.py

--- 코딩테스트준비-기초/그래프와 BFS-BFS.py
+++ 코딩테스트준비-기초/그래프와 BFS-BFS.py
@@ -5,7 +5,10 @@ def bfs(v):
     
     while(lis):
         a = lis.popleft()
-
+        if a == k:
+            print(nums[k])
+            exit()
+        
         if a<=100000-1 and nums[a+1] == 0:
             nums[a+1] = nums[a]+1
             lis.append(a+1)
@@ -17,16 +20,60 @@ def bfs(v):
             lis.append(2*a)
             
 n,k = map(int, input().split())
-
+if n>k:
+    print(n-k)
+    exit()
 nums = [0 for _ in range(100001)]
 
 bfs(n)
-
-print(nums[k])
 
 
 
 # 13913번 - 숨바꼭질 4
 # 14226번 - 이모티콘
 # 13549번 - 숨바꼭질 3
+from collections import deque
+def bfs(v):
+    lis = deque([v])
+    
+    while(lis):
+        a = lis.popleft()
+        if a == k:
+            print(nums[k])
+            exit()
+            
+        if a<=100000//2 and nums[2*a] == -1:
+            if nums[a] == -1:
+                nums[2*a] = 0
+            else:
+                nums[2*a] = nums[a]
+            lis.appendleft(2*a)
+            
+            
+        if a<=100000-1 and nums[a+1] == -1:
+            if nums[a] == -1:
+                nums[a+1] = 1
+            else:
+                nums[a+1] = nums[a]+1
+            lis.append(a+1)
+            
+                        
+        if a>=1 and nums[a-1] == -1:
+            if nums[a] == -1:
+                nums[a-1] = 1
+            else:
+                nums[a-1] = nums[a]+1
+            lis.append(a-1)
+            
+
+n,k = map(int, input().split())
+if n>=k:
+    print(n-k)
+    exit()
+    
+nums = [-1 for _ in range(100001)]
+
+bfs(n)
+
+
 # 1261번 - 알고스팟
